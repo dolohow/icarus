f591aeb3762a4250df758b41e477c1e82ffa10ac
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-01-08 22:37:38 +0100

    admin: Colorize users with active or inactive accounts

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,17 @@
   var dates = document.querySelectorAll('td:nth-child(11)');
   var i;
   var d;
+  var currentDate = new Date();
   for (i=0; i<dates.length; i++) {
     if (dates[i].innerText !== '') {
       d = new Date(dates[i].innerText);
       dates[i].innerText = d.getFullYear() + '/' + parseInt(d.getMonth()+1) +
       '/' + d.getDate();
+      if (d > currentDate) {
+        dates[i].parentNode.style.color = 'green';
+      } else {
+        dates[i].parentNode.style.color = 'red';
+      }
     }
   }
 })();
