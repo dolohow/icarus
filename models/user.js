f28ed3dd3e8c17b4aed8caffc24448de709e8814
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-02-04 23:33:20 +0100

    model: Implement static method addTransfer

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
@@ -9,6 +11,13 @@ var userSchema = new mongoose.Schema({
     index: {unique: true}
   },
   notes: String,
+  accountNumbers: [{type: String}],
+  transfers: [{
+    date: Date,
+    title: String,
+    sender: String,
+    amount: Number
+  }],
   money: {type: Number, default: 0},
   accounts: [{
     username: String,
@@ -26,5 +35,40 @@ var userSchema = new mongoose.Schema({
   }]
 });
 
+userSchema.statics.addTransfer = function (data, callback) {
+  callback = (typeof callback === 'function') ? callback : function() {};
+  this.findOne({accountNumbers: data.accountNumber}, function (err, user) {
+    if (!data.hasOwnProperty('date') ||
+      !data.hasOwnProperty('title') ||
+      !data.hasOwnProperty('sender') ||
+      !data.hasOwnProperty('amount')) {
+      return callback('Transfer incomplete');
+    }
+
+    if (!user) {
+      return callback('No users found');
+    }
+
+    for (var i = 0; i < user.transfers.length; i++) {
+      if (user.transfers[i].date.toLocaleDateString() ===
+        new Date(data.date).toLocaleDateString() &&
+        user.transfers[i].title === data.title &&
+        user.transfers[i].sender === data.sender &&
+        user.transfers[i].amount === data.amount) {
+        return callback('Transfer exists');
+      }
+    }
+
+    user.transfers.push(data);
+    user.save(function (err) {
+      if (err) {
+        callback(err);
+      }
+      callback(null, user);
+    });
+  });
+};
+
 var Users = mongoose.model('Users', userSchema);
+
 module.exports = Users;
---
+++ tests/unit/models/user-test.js
@@ -0,0 +1,94 @@
+'use strict';
+var assert = require('chai').assert;
+var mongoose = require('mongoose');
+var async = require('async');
+
+var User = require('../../../models/user.js');
+
+var correctData = {
+  date: '2015-01-02',
+  title: 'title',
+  sender: 'sender',
+  accountNumber: '13',
+  amount: 10.00
+};
+
+describe('Model User', function () {
+  describe('Transfers', function () {
+    before(function (done) {
+      mongoose.createConnection('mongodb://localhost/db-test');
+      done();
+    });
+    after(function (done) {
+      mongoose.disconnect();
+      done();
+    });
+    beforeEach(function (done) {
+      mongoose.connection.db.dropDatabase();
+      var user = new User({
+        user: 'test@test.com',
+        accountNumbers: [
+          '13'
+        ]
+      });
+      user.save(function () {
+        done();
+      });
+    });
+    it('should reject incomplete transfers', function (done) {
+      var data = {
+        title: 'title',
+        accountNumber: '13'
+      };
+      User.addTransfer(data, function (err, user) {
+        assert.equal(err, 'Transfer incomplete');
+        assert.isUndefined(user);
+        done();
+      })
+    });
+    it('should populate err when user not found', function (done) {
+      var data = {
+        date: '2015-01-02',
+        title: 'title',
+        sender: 'sender',
+        accountNumber: '14',
+        amount: 10.00
+      };
+      User.addTransfer(data, function (err, user) {
+        assert.equal(err, 'No users found');
+        assert.isUndefined(user);
+        done();
+      })
+    });
+    it('should add new transfer when account number matches', function (done) {
+      User.addTransfer(correctData, function (err, user) {
+        assert.property(user.transfers[0], 'title', 'title');
+        assert.lengthOf(user.transfers, 1);
+        done();
+      });
+    });
+    it('should not add the same transfer', function (done) {
+      async.series([
+          function (callback) {
+            User.addTransfer(correctData, function () {
+              callback();
+            })
+          },
+          function (callback) {
+            User.addTransfer(correctData, function (err) {
+              callback(err);
+            })
+          }
+        ],
+        function (err) {
+          assert.equal(err, 'Transfer exists');
+          User.findOne({user: 'test@test.com'}, function (err, user) {
+            assert.property(user.transfers[0], 'title', 'title');
+            assert.lengthOf(user.transfers, 1);
+            done();
+          });
+        }
+      )
+    })
+  });
+});
