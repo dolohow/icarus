04d2f27f1bdd3ad5ca3b0bfefef4b02547c7384c
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-02-15 14:57:51 +0100

    flightplan: Introduce passenger
    We don't want to have any downtimes due to node app crashes. Moreover
    the passenger is able to load balance, spawn new processes and provide some
    security.
    
    https://github.com/phusion/passenger/wiki/Phusion-Passenger%3A-Node.js-tutorial

--- flightplan.js
+++ flightplan.js
@@ -20,25 +20,39 @@ plan.target('production', [{
 }]);
 
 plan.local(function (local) {
+  local.transfer('config/nginx.conf', '/etc/nginx/nginx.conf');
   local.transfer('config/nginx-node-proxy', '/etc/nginx/sites-enabled/');
 });
 
 plan.remote(function (remote) {
-  var nodeVersion = 'v0.10.35';
-
+  /* MongoDB */
   remote.exec('apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10');
   remote.exec('echo \'' +
   'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen\'' +
   ' | sudo tee /etc/apt/sources.list.d/mongodb.list');
-  remote.exec('aptitude update');
-  remote.exec('aptitude install git make gcc g++ sudo nginx python2.7 ' +
-  'mongodb-org -y');
+
+  /* NodeJS */
+  var nodeVersion = 'v0.10.35';
   remote.exec('wget http://nodejs.org/dist/' + nodeVersion + '/node-' +
   nodeVersion + '-linux-x64.tar.gz');
   remote.exec('tar -xzf node-' + nodeVersion + '-linux-x64.tar.gz');
   remote.exec('cp -r node-' + nodeVersion + '-linux-x64 /opt/node');
   //remote.exec('echo PATH=$PATH:/opt/node/bin > ~/.profile');
   remote.exec('PATH=$PATH:/opt/node/bin npm install -g grunt-cli');
+
+  /* Passenger */
+  remote.exec('apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ' +
+  '561F9B9CAC40B2F7');
+  remote.exec('echo deb ' +
+  'https://oss-binaries.phusionpassenger.com/apt/passenger wheezy main > ' +
+  '/etc/apt/sources.list.d/passenger.list');
+  remote.exec('chown root: /etc/apt/sources.list.d/passenger.list');
+  remote.exec('chmod 600 /etc/apt/sources.list.d/passenger.list');
+
+  /* Install them all */
+  remote.exec('aptitude update');
+  remote.exec('aptitude install git make gcc g++ sudo python2.7 mongodb-org ' +
+  'apt-transport-https ca-certificates nginx-extras passenger -y');
 });
 
 plan.remote('deploy', function (remote) {
@@ -47,6 +61,7 @@ plan.remote('deploy', function (remote) {
   remote.exec('echo PATH=$PATH:/opt/node/bin > ~/.profile');
   remote.exec('cd icarus ; ' +
               'PATH=$PATH:/opt/node/bin PYTHON=python2.7 npm install');
+  remote.exec('/home/shell/icarus/tmp/restart.txt');
 });
 
 plan.local('deploy', function (local) {
