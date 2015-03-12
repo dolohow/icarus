c82353262e47a780c45f8f5139e79f0ea1285c15
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-03-12 01:42:13 +0100

    flightplan: Fix bugs discover while testing on a new server

--- flightplan.js
+++ flightplan.js
@@ -7,17 +7,12 @@ plan.target('production', [{
   privateKey: process.env.HOME + '/.ssh/id_rsa'
 }]);
 
-plan.local(function (local) {
-  local.transfer('config/nginx.conf', '/etc/nginx/nginx.conf');
-  local.transfer('config/nginx-node-proxy', '/etc/nginx/sites-enabled/');
-});
-
 plan.remote('install-dep', function (remote) {
   /* MongoDB */
   remote.exec('apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10');
   remote.exec('echo \'' +
   'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen\'' +
-  ' | sudo tee /etc/apt/sources.list.d/mongodb.list');
+  ' | tee /etc/apt/sources.list.d/mongodb.list');
 
   /* NodeJS */
   var nodeVersion = 'v0.12.0';
@@ -26,10 +21,10 @@ plan.remote('install-dep', function (remote) {
   remote.exec('tar -xzf node-' + nodeVersion + '-linux-x64.tar.gz -C /opt');
   remote.exec('mv /opt/node-' + nodeVersion + '-linux-x64 /opt/node');
   remote.exec('echo PATH=$PATH:/opt/node/bin > ~/.profile');
-  remote.exec('PATH=$PATH:/opt/node/bin npm install -g grunt-cli');
+  remote.exec('rm node-' + nodeVersion + '-linux-x64.tar.gz');
 
   /* Node deps */
-  remote.exec('npm install -g bower');
+  remote.exec('PATH=$PATH:/opt/node/bin npm install -g grunt-cli bower');
 
   /* Passenger */
   remote.exec('apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ' +
@@ -39,6 +34,7 @@ plan.remote('install-dep', function (remote) {
   '/etc/apt/sources.list.d/passenger.list');
   remote.exec('chown root: /etc/apt/sources.list.d/passenger.list');
   remote.exec('chmod 600 /etc/apt/sources.list.d/passenger.list');
+  //remote.exec('aptitude install apt-transport-https -y');
 
   /* Install them all */
   remote.exec('aptitude update');
@@ -46,15 +42,18 @@ plan.remote('install-dep', function (remote) {
   'apt-transport-https ca-certificates nginx-extras passenger -y');
 });
 
+plan.local('install-dep', function (local) {
+  local.transfer('config/nginx.conf', '/etc/nginx/nginx.conf');
+  local.transfer('config/nginx-node-proxy', '/etc/nginx/sites-enabled/');
+});
+
 plan.remote('deploy', function (remote) {
   remote.exec('rm -rf icarus');
   remote.exec('git clone https://github.com/dolohow/icarus.git');
   remote.exec('echo PATH=$PATH:/opt/node/bin > ~/.profile');
-  remote.exec('cd icarus ; ' +
-  'PATH=$PATH:/opt/node/bin PYTHON=python2.7 npm install ; ' +
-  'bower install' +
-  'mkdir -p tmp ; ' +
-  'touch tmp/restart.txt');
+  remote.exec('cd icarus;' +
+  'PATH=$PATH:/opt/node/bin PYTHON=python2.7 npm install;' +
+  'PATH=$PATH:/opt/node/bin bower install;');
 });
 
 plan.local('deploy', function (local) {
