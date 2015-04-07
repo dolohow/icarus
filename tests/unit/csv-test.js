703a56eff216f6576906311da89e209ee64a76f7
Łukasz Żarnowiecki <dolohow@outlook.com>
2015-04-08 01:02:27 +0200

    csv: Unify output from PayPal parser with mbank

--- tests/unit/csv-test.js
+++ tests/unit/csv-test.js
@@ -33,7 +33,32 @@ describe('CSV Parser', function () {
         assert.lengthOf(data, 0);
         done();
       });
-    })
+    });
+    it('should create proper array of objects', function (done) {
+      var string = '2010-09-10;;;;;;;;\n';
+      csv.parse.mbank(string, function (err, data) {
+        assert.property(data[0], 'date');
+        assert.property(data[0], 'title');
+        assert.property(data[0], 'sender');
+        assert.property(data[0], 'accountNumber');
+        assert.property(data[0], 'amount');
+        done();
+      });
+    });
+    it('should remove quotes from account number', function (done) {
+      var string = '2010-09-10;;;;;\'123\';;;\n';
+      csv.parse.mbank(string, function (err, data) {
+        assert.equal(data[0].accountNumber, '123');
+        done();
+      });
+    });
+    it('should set amount to float', function (done) {
+      var string = '2010-09-10;;;;;;25,25;;\n';
+      csv.parse.mbank(string, function (err, data) {
+        assert.equal(data[0].amount, 25.25);
+        done();
+      });
+    });
   });
   describe('paypal', function () {
     it('should ignore the first header/line', function (done) {
@@ -42,16 +67,48 @@ describe('CSV Parser', function () {
       csv.parse.paypal(string, function (err, data) {
         assert.lengthOf(data, 1);
         done();
-      })
+      });
     });
-    it('should create err obj with prop message when different rows', function (done) {
-      var string = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n' +
-        ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
+    it('should create err obj with prop message when different rows',
+      function (done) {
+        var string = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n' +
+          ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
+        csv.parse.paypal(string, function (err, data) {
+          assert.isUndefined(data);
+          assert.isDefined(err.message);
+          done();
+        });
+      });
+    it('should create proper array of objects', function (done) {
+      var string = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n' +
+        ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
       csv.parse.paypal(string, function (err, data) {
-        assert.isUndefined(data);
-        assert.isDefined(err.message);
+        assert.property(data[0], 'date');
+        assert.property(data[0], 'title');
+        assert.property(data[0], 'sender');
+        assert.property(data[0], 'accountNumber');
+        assert.property(data[0], 'amount');
         done();
-      })
+      });
     });
-  })
+    it('should remove double quotes from transfer properties', function (done) {
+      var string = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n' +
+        '"",,,"",,,,,"a",,,"","",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
+      csv.parse.paypal(string, function (err, data) {
+        assert.equal(data[0].date, '');
+        assert.equal(data[0].title, '');
+        assert.equal(data[0].sender, '');
+        assert.equal(data[0].accountNumber, '');
+        done();
+      });
+    });
+    it('should set amount to float', function (done) {
+      var string = ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n' +
+        ',,,,,,,,"25,25",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,';
+      csv.parse.paypal(string, function (err, data) {
+        assert.equal(data[0].amount, 25.25);
+        done();
+      });
+    });
+  });
 });
